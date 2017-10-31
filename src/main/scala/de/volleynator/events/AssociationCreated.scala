3c12ca25d48327dc78cc5d6e5439ee87afa43c9b
Eddy Bobbin <eddykaya99@googlemail.com>
2017-10-31 15:38:54 +0100

    associationCreated can now also be a tagged, bumped version to 0.4.0

--- src/main/scala/de/volleynator/events/AssociationCreated.scala
+++ src/main/scala/de/volleynator/events/AssociationCreated.scala
@@ -1,11 +1,17 @@
 package de.volleynator.events
 
+import akka.persistence.journal.Tagged
+
 case class AssociationCreated(id: String, name: String) {
   require(id != null)
   require(name != null)
 
   def +(association: AssociationCreated): AssociationCreated = {
     association
+  }
+
+  def tagged: Tagged = {
+    Tagged(this, Set(id,"Association", "AssociationCreated"))
   }
 
 }
