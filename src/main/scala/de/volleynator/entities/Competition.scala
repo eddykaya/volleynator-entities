420a1edd548839750512d6db1a8be77e1be40479
Eddy Bobbin <eddykaya99@googlemail.com>
2018-10-06 13:23:21 +0200

    new event CompetitionUpdated to know when a competition was updated last

--- src/main/scala/de/volleynator/entities/Competition.scala
+++ src/main/scala/de/volleynator/entities/Competition.scala
@@ -1,11 +1,14 @@
 package de.volleynator.entities
 
+import java.time.LocalDateTime
+
 case class Competition(
                         name: String,
                         season: String,
                         active: Boolean,
                         leagueUrl: String,
                         matchPlanUrl: String,
-                        leagueName: String
+                        leagueName: String,
+                        updatedAt: LocalDateTime
                       )
 
